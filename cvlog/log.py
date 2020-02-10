023f6f81e0fda31e77171ea45282fb3b994989f6
Andrey Chertkov <andreychertckov@gmail.com>
2020-02-10 10:27:03 +0300

    Add description to logs

--- cvlog/log.py
+++ cvlog/log.py
@@ -1,29 +1,35 @@
-import cv2
-import os
-import cvlog.html_logger as hl
 import base64
-from cvlog.config import Config,Mode,Level
-html_logger= None
+import os
 
-def image(level,image):
-    if image is None:
+import cv2
+
+import cvlog.html_logger as hl
+from cvlog.config import Config, Mode
+import time
+
+html_logger = None
+
+
+def image(level, image=None, description=None):
+    if image is None and description is None:
         return
     try:
         __init()
         if Config().curent_level().value < level.value:
             return
         if Config().curent_mode() == Mode.DEBUG:
-            show_image(level.name,image)
+            show_image(level.name, image)
         elif Config().curent_mode() == Mode.LOG:
-            log_image(level.name,image)
+            log_image(level.name, image, description)
     except Exception as e:
         print(e)
 
-def log_image(level,img):
+
+def log_image(level, img, description):
     retval, buffer = cv2.imencode('.png', img)
     if not retval:
         return None
-    html_logger.log_image(level,base64.b64encode(buffer).decode())
+    html_logger.log_image(level, base64.b64encode(buffer).decode(), description)
 
 
 def show_image(title, img):
@@ -35,7 +41,8 @@ def show_image(title, img):
         os._exit(1)
     return value
 
+
 def __init():
     global html_logger
-    if Config().curent_mode()==Mode.LOG and html_logger is None:
-        html_logger=hl.HtmlLogger(Config().log_path()+"/log.html")
+    if Config().curent_mode() == Mode.LOG and html_logger is None:
+        html_logger = hl.HtmlLogger(Config().log_path() + f"/log_{time.now()}.html")
